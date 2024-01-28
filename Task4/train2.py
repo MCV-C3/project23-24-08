b1bdf7ae3b14dc81ba0059012e6b5c6b59470edf
Luis Gonzalez <luis.gonzalez.gudino@estudiantat.upc.edu>
2024-01-28 10:00:38 +0100

    changes

--- Task4/train2.py
+++ Task4/train2.py
@@ -1,27 +1,79 @@
-import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Dropout, BatchNormalization
+from tensorflow.keras import layers
 from tensorflow.keras.models import Model
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.optimizers import Adam, SGD
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-from keras.utils.layer_utils import count_params  
+import tensorflow as tf
+from tensorflow.python.keras.utils.layer_utils import count_params  
 from keras.utils import plot_model
 import wandb
-from utils import build_model
+from utils import build_model, preprocess, get_dataset
 
-
-def train(config=None):
+def train(config=None, pretrain=False):
 
     if sweep:
-        wandb.init()
-        # Get hyperparameters
-        config = wandb.config
+         wandb.init()
+         # Get hyperparameters
+         config = wandb.config
 
     # Define constants
     IMG_WIDTH, IMG_HEIGHT = config['resolution'], config['resolution']
-    MODEL_PATH = './pretrained/model_no_pretrain.h5'
-    DATASET_DIR = '../MIT_small_train_1'
 
+    auc = tf.keras.metrics.AUC(num_thresholds=200, name='PR-AUC', curve='PR')
+    final_learning_rate = 1e-5
+    learning_rate_decay_factor = (final_learning_rate / config['lr'])**(1/config['epochs'])
+
+    if pretrain:
+        MODEL_PATH = './C3/pretrained/cifar10_pretrained.h5'
+
+        es_cback = EarlyStopping(monitor='val_accuracy', mode='max', patience=7, min_delta=0.0001)
+        checkpoint_cback = ModelCheckpoint(filepath=MODEL_PATH, mode='max', monitor='val_accuracy', save_best_only=True, save_weights_only=True)
+        cbacks = [es_cback, checkpoint_cback]
+
+        train_dataset, test_dataset = get_dataset(config['batch_size'])
+
+        lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+            initial_learning_rate=config['lr'],
+            decay_steps=int(len(train_dataset)/config['batch_size']),
+            decay_rate=learning_rate_decay_factor,
+            staircase=True)
+
+        model = build_model(config, num_classes=10)  # Ensure model is built for 10 classes
+
+        # Compile the model
+        model.compile(optimizer=Adam(learning_rate=lr_schedule, weight_decay=config['l2']),
+                    loss='categorical_crossentropy',
+                    metrics=['accuracy', auc])
+        
+        model.fit(train_dataset, 
+                  batch_size=config['batch_size'], 
+                  epochs=config['epochs'], 
+                  validation_data=test_dataset,
+                  callbacks=cbacks)
+
+        # Load the trained model
+        model.load_weights(MODEL_PATH)
+
+        # Modify the final layer to have 8 channels
+        x = model.layers[-4].output
+        x = layers.Conv2D(8, kernel_size=3, strides=1, padding='same')(x)
+        x = layers.GlobalAveragePooling2D()(x)
+        x = layers.Activation('softmax')(x)
+        model = Model(inputs=model.input, outputs=x)
+
+        # Update model path
+        MODEL_PATH = './C3/pretrained/model_finetuned.h5'
+    else:
+        # Update model path
+        MODEL_PATH = './C3/pretrained/model_no_pretrain.h5'
+        # Load model with 8-channel last layer
+        model = build_model(config, num_classes=8)
+
+    es_cback = EarlyStopping(monitor='val_accuracy', mode='max', patience=15, min_delta=0.0001)
+    checkpoint_cback = ModelCheckpoint(filepath=MODEL_PATH, mode='max', monitor='val_accuracy', save_best_only=True, save_weights_only=True)
+    cbacks = [es_cback, checkpoint_cback]
+
+    DATASET_DIR = '/export/home/mcv/datasets/C3/MIT_small_train_1/'
     # Define the data generator for data augmentation and preprocessing
     train_data_generator = ImageDataGenerator(
         preprocessing_function=lambda x: x/255.0,
@@ -57,43 +109,24 @@ def train(config=None):
 
     # Load and preprocess the test dataset
     test_dataset = test_data_generator.flow_from_directory(
-        directory='../MIT_split/test/',
+        directory= '/export/home/mcv/datasets/C3/MIT_split/test/',
         target_size=(IMG_WIDTH, IMG_HEIGHT),
         batch_size=config['batch_size'],
         class_mode='categorical',
         shuffle=False  # No need to shuffle the test data
     )
-    
-    model = build_model(config)
-    # plot_model(model, to_file='model.png', show_shapes=True, show_layer_names=True)
-    print(model.summary())
-
-    final_learning_rate = 1e-5
-    learning_rate_decay_factor = (final_learning_rate / config['lr'])**(1/config['epochs'])
-    steps_per_epoch = int(train_dataset.samples/config['batch_size'])
 
     lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
         initial_learning_rate=config['lr'],
-        decay_steps=steps_per_epoch,
+        decay_steps=int(train_dataset.samples/config['batch_size']),
         decay_rate=learning_rate_decay_factor,
         staircase=True)
-    
-    es_cback = EarlyStopping(monitor='val_accuracy', mode='max', patience=15, min_delta=0.0001)
-    checkpoint_cback = ModelCheckpoint(filepath=MODEL_PATH, mode='max', monitor='val_accuracy', save_best_only=True, save_weights_only=True)
-    cbacks = [es_cback, checkpoint_cback]
-
-    # if config['optimizer_type'] == 'adam':
-    optimizer = Adam(learning_rate=lr_schedule, decay=config['l2'])
-    # elif config['optimizer_type'] == 'sgd':
-    #     optimizer = SGD(learning_rate=lr_schedule, momentum=config['momentum'], decay=config['l2'])
-
-    auc = tf.keras.metrics.AUC(num_thresholds=200, name='PR-AUC', curve='PR')
 
     # Compile the model
-    model.compile(optimizer=optimizer,
+    model.compile(optimizer=Adam(learning_rate=lr_schedule, weight_decay=config['l2']),
                 loss='categorical_crossentropy',
                 metrics=['accuracy', auc])
-
+                
     # Train the model
     history = model.fit(
         train_dataset,
@@ -101,14 +134,14 @@ def train(config=None):
         validation_data=val_dataset,
         callbacks=cbacks
     )
-
+    
     for epoch in range(len(history.history['loss'])):
         wandb.log({
             'train_loss': history.history['loss'][epoch],
             'train_accuracy': history.history['accuracy'][epoch],
             'val_loss': history.history['val_loss'][epoch],
             'val_accuracy': history.history['val_accuracy'][epoch]
-        })
+            })
     wandb.log({'n_params': count_params(model.trainable_weights)})
 
     # Load the trained model
@@ -123,7 +156,7 @@ def train(config=None):
     wandb.finish()
 
 
-sweep = True
+sweep = False
 if sweep:
     sweep_id = "c3-mcv/cnn2/p6drmh0y"
     wandb.agent(sweep_id, train, count=2)
@@ -133,22 +166,22 @@ else:
         'lr': 1e-3,
         'batch_size': 32,
         'epochs': 50,
-        'activation': 'relu',
+        'activation': 'gelu',
         'optimizer_type': 'adam',
-        'dropout': 0.3,
+        'dropout': 0.25,
         'use_batch_norm': True,
-        'l2': 0.01,
+        'l2': 0.001,
         'resolution': 256,
-        'n_conv_blocks': 1,
+        'n_conv_blocks': 2,
         'filters_0': 64,
-        'filters_sq_1': 16,
-        'filters_ex_1': 64,
-        'filters_sq_2': 32,
-        'filters_ex_2': 128,
+        'filters_sq_1': 64,
+        'filters_ex_1': 128,
+        'filters_sq_2': 64,
+        'filters_ex_2': 512,
     }
 
     # Initialize wandb with a sample configuration
     wandb.init(project='cnn2', entity='c3-mcv', config=config)
 
     # Train the model
-    train(config)
+    train(config, pretrain=True)
--- Task4/utils.py
+++ Task4/utils.py
@@ -1,123 +1,57 @@
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D, Dense, Dropout, BatchNormalization, Concatenate, Input, Activation
+from tensorflow.keras import layers
 from tensorflow.keras.models import Model
+import tensorflow as tf
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.datasets import cifar10
 
-def conv_block(model, filters, kernel_size, stride, activation, use_batch_norm, d, id_layer):
-    """
-    Add a convolutional block to a Keras model, consisting of Conv2D, BatchNormalization (optional) and MaxPooling2D.
+def squeeze_block(x, squeeze_planes, expand1x1_planes, expand3x3_planes, activation):
+    squeeze = layers.Conv2D(squeeze_planes, kernel_size=1, activation=activation)(x)
 
-    Args:
-    model: A Keras model instance.
-    filters (int): Number of filters.
-    kernel_size (int): Kernel size.
-    activation (str): Activation function.
-    use_batch_norm (bool): Whether to use BatchNormalization.
-    d (float): Dropout rate.
-    id_layer (int): Layer ID.
+    expand1x1 = layers.Conv2D(expand1x1_planes, kernel_size=1, activation=activation)(squeeze)
+    expand3x3 = layers.Conv2D(expand3x3_planes, kernel_size=3, padding='same', activation=activation)(squeeze)
 
-    Returns:
-    model: A Keras model instance.
-    """
-    model.add(Conv2D(filters=filters, kernel_size=kernel_size, activation=activation, padding='same', strides=1))
-    model.add(Conv2D(filters=filters, kernel_size=kernel_size, activation=activation, padding='same', strides=stride))
-    if id_layer % 2 == 0:
-        model.add(MaxPooling2D(pool_size=2))
-    if d > 0:
-        model.add(Dropout(d))
-    if use_batch_norm:
-        model.add(BatchNormalization())
+    return tf.concat([expand1x1, expand3x3], axis=3)
 
-    return model
+def build_model(config, num_classes):
+    inputs = layers.Input(shape=(config['resolution'], config['resolution'], 3)) 
 
-def build_model(config):
-    """
-    Build a customizable CNN model with Global Average Pooling.
+    x = layers.Conv2D(config['filters_0'], kernel_size=3, strides=1, padding='same', activation=config['activation'])(inputs)
+    x = layers.MaxPooling2D(pool_size=2, strides=2)(x)
+    
+    if config['dropout'] > 0:
+        x = layers.Dropout(config['dropout'])(x)
+    if config['use_batch_norm']:
+        x = layers.BatchNormalization()(x)
 
-    Args:
-    config (dict): A dictionary containing model configuration parameters.
-
-    Returns:
-    model: A Keras model instance.
-    """
-
-    model = Sequential()
-
-    # First layer with input shape specified
-    model.add(Conv2D(filters=config['filters_0'], kernel_size=3, strides=1, activation=config['activation'], padding='same', input_shape=(config['resolution'], config['resolution'], 3)))
-
-    n_conv_blocks = config['n_conv_blocks']
-
-    # Add convolutional blocks
     for i in range(config['n_conv_blocks']):
-        model = conv_block(model, config[f'filters_{i+1}'], config[f'size_{i+1}'], config[f'stride_{i+1}'], config['activation'], config['use_batch_norm'], config['dropout'], i+1)
-    # Global Average Pooling layer
-    model.add(GlobalAveragePooling2D())
-
-    neurons = config[f'filters_{n_conv_blocks}']
-
-    # Add dense layers
-    for i in range(config['n_dense_layers']):
-        model.add(Dense(neurons, activation=config['activation2']))
+        x = squeeze_block(x, config[f'filters_sq_{i+1}'], config[f'filters_ex_{i+1}'], config[f'filters_ex_{i+1}'], config['activation'])
+        x = layers.MaxPooling2D(pool_size=2, strides=2)(x)
+        
         if config['dropout'] > 0:
-            model.add(Dropout(config['dropout']))
+            x = layers.Dropout(config['dropout'])(x)
         if config['use_batch_norm']:
-            model.add(BatchNormalization())
-        neurons //= 2
+            x = layers.BatchNormalization()(x)
 
-    # Output layer
-    model.add(Dense(8, activation='softmax'))
+    x = layers.Conv2D(num_classes, kernel_size=3, strides=1, padding='same')(x)
+    x = layers.GlobalAveragePooling2D()(x)
+    x = layers.Activation('softmax')(x)
 
-    return model
-
-def conv_block2(x, squeeze_filters, expand_filters, activation):
-    """
-    Creates a convolution block.
-
-    Args:
-    x: Input tensor.
-    squeeze_filters (int): Number of filters in the squeeze layer.
-    expand_filters (int): Number of filters in the expand layer.
-
-    Returns:
-    x: Output tensor.
-    """
-    # Squeeze layer
-    squeeze = Conv2D(squeeze_filters, (1, 1), padding='same', activation=activation)(x)
+    return Model(inputs=inputs, outputs=x)
     
-    # Expand layer (with a mix of 1x1 and 3x3 convolutions)
-    expand_1x1 = Conv2D(expand_filters, (1, 1), padding='same', activation=activation)(squeeze)
-    expand_3x3 = Conv2D(expand_filters, (3, 3), padding='same', activation=activation)(squeeze)
-    
-    # Concatenate 1x1 and 3x3 expand outputs
-    x = Concatenate()([expand_1x1, expand_3x3])
+def preprocess(image, label):
+    image = tf.image.resize(image, [256, 256])
+    image = image / 255.0
+    return image, label
 
-    return x
+def get_dataset(batch_size):
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    y_train, y_test = to_categorical(y_train, 10), to_categorical(y_test, 10)
 
-def build_model2(config):
-    """
+    train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+    train_dataset = train_dataset.map(preprocess).batch(batch_size)
 
-    Args:
+    test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+    test_dataset = test_dataset.map(preprocess).batch(batch_size)
 
-    Returns:
-    model: A Keras model instance.
-    """
-    inputs = Input((config['resolution'], config['resolution'], 3))
-
-    # Initial convolution layer
-    x = Conv2D(config['filters_0'], kernel_size=1, strides=1, padding='same', activation=config['activation'])(inputs)
-    x = MaxPooling2D(pool_size=2)(x)
-
-    for i in range(config['n_conv_blocks']):
-        x = conv_block2(x, squeeze_filters=config[f'filters_{i+1}'], expand_filters=config[f'filters_{i+1}'], activation=config['activation'])
-        x = conv_block2(x, squeeze_filters=config[f'filters_{i+1}'], expand_filters=config[f'filters_{i+1}'], activation=config['activation'])
-        if (i+1) % 2 == 0:
-            x = MaxPooling2D(pool_size=2)(x)
-
-    # Final layers
-    x = Conv2D(8, (1, 1), padding='same', activation=config['activation2'])(x)
-    x = GlobalAveragePooling2D()(x)
-    x = Activation('softmax')(x)
-
-    model = Model(inputs, x)
-    return model
-
+    return train_dataset, test_dataset
+     
